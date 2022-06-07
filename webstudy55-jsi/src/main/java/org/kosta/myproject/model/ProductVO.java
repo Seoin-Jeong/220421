e4a32e20c2c8cde235fe7a3515f3d27fca347f71
Seoin-Jeong <seoin0529@gmail.com>
2022-06-07 23:41:56 +0900

    branch test1

--- webstudy55-jsi/src/main/java/org/kosta/myproject/model/ProductVO.java
+++ webstudy55-jsi/src/main/java/org/kosta/myproject/model/ProductVO.java
@@ -1,5 +1,17 @@
 package org.kosta.myproject.model;
 
 public class ProductVO {
-
+	private String id;
+	private String password;
+	private String address;
+	public ProductVO() {
+		super();
+	}
+	public ProductVO(String id, String password, String address) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.address = address;
+	}
+	
 }
